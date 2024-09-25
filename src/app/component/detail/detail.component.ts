f680f5cfe7026ee2292cbab29cabcb920f9f5016
Hank Lingle <Hank.Lingle@eblusolutions.com>
2024-09-25 09:15:45 -0400

    feat: empty block instead of if else. unsubscribe from obs using take until destroyed

--- src/app/component/detail/detail.component.ts
+++ src/app/component/detail/detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { JobService } from '../../service/job.service';
 import { JobDetailModel } from '../../model/job-detail';
@@ -10,18 +11,14 @@ import { JobDetailModel } from '../../model/job-detail';
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.css'
 })
-export class DetailComponent implements OnInit {
+export class DetailComponent {
   constructor(private readonly jobService: JobService, private readonly route: ActivatedRoute){
-    //empty
-  }
-
-  data: JobDetailModel = new JobDetailModel();
-
-  ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.pipe(takeUntilDestroyed()).subscribe(params => {
       this.jobService.getJob(params['id']).subscribe(
         response => this.data = response
       );
     })
   }
+
+  data: JobDetailModel = new JobDetailModel();
 }
